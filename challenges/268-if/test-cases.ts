050956115b51212e40fc316dbf4a490130ab7cf1
马浩 <303527871@qq.com>
2022-05-23 17:21:00 +0800

    if

---
+++ challenges/268-if/test-cases.ts
@@ -0,0 +1,13 @@
+type If<C extends (true | false), T, F> = C extends true ? T : F;
+
+
+
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
+  Expect<Equal<If<false, 'a', 2>, 2>>,
+]
+
+// @ts-expect-error
+type error = If<null, 'a', 'b'>
